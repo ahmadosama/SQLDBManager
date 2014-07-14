3d8016b824e8a0c0a27a9d3398bf61f204141d3a
Ahmad Osama <ahmad.osama1984@gmail.com>
2014-07-14 11:49:57 +0530

    initial commit

---
+++ SQLDBManager/Genericsql.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace SQLDBManager
+{
+    public class Genericsql
+    {
+        public Genericsql() { }
+        public DataTable databaselist(QueryExecution qe)
+        {
+            try
+            {
+                string _querytolistdatabases = "Select name,CASE is_cdc_enabled WHEN 0 Then 'False' WHEN 1 THEN 'True' END AS is_cdc_enabled from " +
+                   " sys.databases where database_id>4 and state_desc='Online' order by is_cdc_enabled desc ";
+                DataTable _dtdbs = qe.fn_ExecuteTable(_querytolistdatabases);
+                return _dtdbs;
+            }catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
